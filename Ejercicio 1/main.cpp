ea73f0ce843e185c5047504ff2279eb3a600e4f3
Gmongtor <gmongtor@myuax.com>
2023-11-15 19:25:58 +0100

    Commit 1 1

---
+++ Ejercicio 1/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+namespace Matematicas {
+    int suma(int a, int b) {
+        return a + b;
+    }
+    int resta(int a, int b) {
+        return a - b;
+    }
+    int multiplicacion(int a, int b) {
+        return a * b;
+    }
+    int division(int a, int b) {
+        return a / b;
+    }
+
+}
