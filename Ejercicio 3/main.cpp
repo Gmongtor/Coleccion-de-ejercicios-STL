d248a59db22e60fe484264f965d95ff1f0622009
Gmongtor <gmongtor@myuax.com>
2023-11-15 20:39:43 +0100

    commit 3 1

---
+++ Ejercicio 3/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+namespace Geometria {
+    class Circulo {
+    private:
+        double radio;
+    public:
+        Circulo(double r) : radio(r)[]
+        double calcularArea(){
+            return 3.14 * radio * radio;
+        }
+        double calcularPerimetro(){
+            return 2 * 3.14 * radio;
+        }
+    };
+}
