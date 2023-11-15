dfb91ae3fccf991cac7f8bd5e291259187d54751
Gmongtor <gmongtor@myuax.com>
2023-11-15 19:29:09 +0100

    Commit 1 2

--- Ejercicio 1/main.cpp
+++ Ejercicio 1/main.cpp
@@ -12,5 +12,15 @@ namespace Matematicas {
     int division(int a, int b) {
         return a / b;
     }
-
+    int cuadrado(int a) {
+        return a * a;
+    }
+}
+int main() {
+    std::cout << "Suma: " << Matematicas::suma(5, 3) << std::endl;
+    std::cout << "Resta: " << Matematicas::resta(5, 3) << std::endl;
+    std::cout << "Multiplicacion: " << Matematicas::multiplicacion(5, 3) << std::endl;
+    std::cout << "Division: " << Matematicas::division(5, 3) << std::endl;
+    std::cout << "Cuadrado: " << Matematicas::cuadrado(5) << std::endl;
+    return 0;
 }
